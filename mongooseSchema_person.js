c14634b695eeecde8c72de938f1dc84890cb7dd9
Alexander <alexandermichaelmiranda@gmail.com>
2013-12-10 16:30:19 -0800

    updated mongodb

--- mongooseSchema_person.js
+++ mongooseSchema_person.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
 var Schema = mongoose.Schema;
-
-var personSchema = new Schema({
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+	var personSchema = new Schema({
 	firstName: String,
 	lastName: String,
 	email: String,
@@ -9,4 +12,23 @@ var personSchema = new Schema({
 	customFeelings: Array,
 	customObservations: Array,
 	customChoices: Array
+});
+	var Person = mongoose.model('Person', personSchema);
+	var alex = new Person({firstName: "Alexander", lastName: "Miranda", email: "amiranda@umich.edu", customNeeds: [], customFeelings: [], customObservations: [], customChoice: []})
+	alex.save(function(err, alex) {
+		if(err){
+			console.log("Error saving person instance " + alex.firstName + " " + alex.lastName);
+		}
+		else {
+			Person.find(function(err, people){
+				if (err){
+					console.log("Could not find collection");
+				}
+				else {
+					console.log(people);
+				}
+
+		})
+		}
+	});
 });
